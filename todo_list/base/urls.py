37c5fa7e325e1e6e05ac91830236fa407500313d
Atul Sharma <41663027+atulsharma2000@users.noreply.github.com>
2021-05-27 13:48:05 +0530

    3

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import tasklist
+from django.urls.resolvers import URLPattern
+from .views import TaskDetail, TaskList, TaskCreate
 
 urlpatterns = [
-    path('', tasklist, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy           #redirects user to certain page
+
 from .models import Task
+
 # Create your views here.
 
-def tasklist(request):
-    
-    
+class TaskList(ListView):
+    model = Task                # this looks for task_list.html
+    context_object_name = 'tasks'       #giving custom name to query set (that was object_list in html)
+
+
+class TaskDetail(DetailView):
+    model = Task                #looks for task_detail.html
+    context_object_name = 'task'
+    template_name = 'base/task.html'  #customizing the deafult task_detail.html to task.html or other
+
+
+class TaskCreate(CreateView):#looks for base/task_form.html
+    model = Task
+    fields = '__all__'      #making all fields['title', 'description']
+    success_url = reverse_lazy('tasks')    #passing the url name 'tasks'
